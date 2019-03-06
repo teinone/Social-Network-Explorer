ddb8a98e129e14a5b2327d16f7b960e13d14176e
CuriousGeorge7265 <47153667+CuriousGeorge7265@users.noreply.github.com>
2019-03-06 16:07:56 +0100

    Update and rename shinydb_sandbox4.R to shinydb_sandbox5.R

--- shinydb_sandbox5.R
+++ shinydb_sandbox5.R
@@ -21,7 +21,8 @@ ui <- dashboardPage(
           sidebarMenu( #This is where you define the left menu objects and their targets. 
             #Note: these need to match the tabItem (tabNames) 
             menuItem("Welcome",  tabName = "Welcome",   icon = icon("dashboard")),
-            menuItem("Network", tabName = "Network",   icon = icon("th"))
+            menuItem("Network", tabName = "Network",   icon = icon("th")),
+            menuItem("Kurzegezagt!", tabName = "Description", icon = icon("th"))
           ) #sidebarMenu end
         ), #dashboardSidebar end
   dashboardBody(
@@ -40,7 +41,7 @@ ui <- dashboardPage(
             sliderInput("slider", "Number of cats involved:", 1, 100, 50) #min, max, default 
             ) #box end
           ) #fluidRow end
-        ), #tab end
+        ), # first tab end
       
       tabItem(tabName = "Network",               # Here we define a new 
         fluidRow(                                # Boxes need to be put in a row (or column)
@@ -53,9 +54,28 @@ ui <- dashboardPage(
             plotlyOutput("plotly1")
            # title = "Cat controls, doesn't do anything yet",
            #  sliderInput("slider", "Number of cats involved:", 1, 100, 50) #min, max, default 
-          ) #box end
+          )#box end
         ) #fluidRow end
-      ) #tab end
+      ),# second tab item end
+      
+      # Start of the 3rd tab 
+      tabItem(tabName = "Description",
+               fluidRow(
+                 # Tab.3, box.1
+                 box(
+                   verbatimTextOutput("description")
+                 ), # end of the box.1
+                 
+                 # Start of the 2nd box 
+                 box(
+                   verbatimTextOutput("instruction")
+                 ) #end of tab.3, box.2
+                         ) #end of the fluidrow 
+        
+      ) #End of the 3rd tab
+       
+      
+      
     ) #tabItems end
   ) #dashboard body end
 ) #dashboardpage end
@@ -98,7 +118,16 @@ server <- function(input, output) {
       if (is.null(event1)) "Hover on a point!" else event1    
   }) # end of output$event block      
     
+   output$description <- renderText({ "The hyperlink network represents the directed connections between two subreddits (a subreddit is a community on Reddit). We also provide subreddit embeddings. The network is extracted from publicly available Reddit data of 2.5 years from Jan 2014 to April 2017.
 
+     Subreddit Hyperlink Network: the subreddit-to-subreddit hyperlink network is extracted from the posts that create hyperlinks from one subreddit to another. We say a hyperlink originates from a post in the source community and links to a post in the target community. Each hyperlink is annotated with three properties: the timestamp, the sentiment of the source community post towards the target community post, and the text property vector of the source post. The network is directed, signed, temporal, and attributed.
+     
+     Note that each post has a title and a body. The hyperlink can be present in either the title of the post or in the body. Therefore, we provide one network file for each."
+     })   # end of the description text 
+
+   
+   output$insruction <- renderText({"test instruction"
+     }) # end of instruction text 
 } #end of server block
 
 shinyApp(ui, server)
