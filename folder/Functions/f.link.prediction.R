21c8714198d75a0791c085d3f2ad197c03f44c2b
teinone <pauli.teinonen@gmail.com>
2019-03-11 21:29:38 +0100

    Link prediction works
    Takes a graph and a dt with the top10 nodes by degree.
    Returns a dt. with a prediction for links between the top 7.

---
+++ folder/Functions/f.link.prediction.R
@@ -0,0 +1,38 @@
+#==============================================================================
+# FUNCTION: Link prediction: This function returns predicted links for the top10 
+#==============================================================================
+# Returns a dt. with the Jaccard similarity of the top7 highest degrees in the 
+# network
+
+f.link.prediction <- function(g.reddit, dt.top10){
+
+  # Predict the links for the top 7 most connected subreddits using Jaccard similarity
+  m.predicted.links <- similarity(g.reddit, vids = c(dt.top10$name[1:7]), method = "jaccard")
+  
+  # Change the column names
+  colnames(m.predicted.links) <- c(dt.top10$name[1:7])
+  
+  #Change the predicted links to a data.table
+  dt.predicted.links <- as.data.table(m.predicted.links)
+  
+  # Add row names
+  dt.predicted.links$rownames <-rownames(dt.predicted.link) <- c(dt.top10$name[1:7])
+  
+  # Create vectors for ordering the columns.
+  c.top7  <- c(dt.top10$name[1:7])
+  c.order <- c("rownames", c.top7) 
+  
+  #Set the order of the columns
+  setcolorder(dt.predicted.links, c.order)
+  
+  return(dt.predicted.links)
+}
+
+# Example use:
+dt.orlando.links <- f.link.prediction(g.orlando.subgraph, dt.orlando.top10)
+View(dt.orlando.links)
+
+
+
+
+
