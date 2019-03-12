31ab1495e6c08fa7bd8a24cacb6ecf189da767a2
yeshipk1989 <46649318+yeshipk1989@users.noreply.github.com>
2019-03-12 15:52:22 +0100

    Changed from 7 to 5 link preditions
    Changed from

--- folder/Functions/f.link.prediction.R
+++ folder/Functions/f.link.prediction.R
@@ -1,26 +1,26 @@
 #==============================================================================
 # FUNCTION: Link prediction: This function returns predicted links for the top10 
 #==============================================================================
-# Returns a dt. with the Jaccard similarity of the top7 highest degrees in the 
+# Returns a dt. with the Jaccard similarity of the top5 highest degrees in the 
 # network
 
 f.link.prediction <- function(g.reddit, dt.top10){
 
-  # Predict the links for the top 7 most connected subreddits using Jaccard similarity
-  m.predicted.links <- similarity(g.reddit, vids = c(dt.top10$name[1:7]), method = "jaccard")
+  # Predict the links for the top 5 most connected subreddits using Jaccard similarity
+  m.predicted.links <- similarity(g.reddit, vids = c(dt.top10$name[1:5]), method = "jaccard")
   
   # Change the column names
-  colnames(m.predicted.links) <- c(dt.top10$name[1:7])
+  colnames(m.predicted.links) <- c(dt.top10$name[1:5])
   
   #Change the predicted links to a data.table
   dt.predicted.links <- as.data.table(m.predicted.links)
   
   # Add row names
-  dt.predicted.links$rownames <-rownames(dt.predicted.links) <- c(dt.top10$name[1:7])
+  dt.predicted.links$rownames <-rownames(dt.predicted.links) <- c(dt.top10$name[1:5])
   
   # Create vectors for ordering the columns.
-  c.top7  <- c(dt.top10$name[1:7])
-  c.order <- c("rownames", c.top7) 
+  c.top5  <- c(dt.top10$name[1:5])
+  c.order <- c("rownames", c.top5) 
   
   #Set the order of the columns
   setcolorder(dt.predicted.links, c.order)
