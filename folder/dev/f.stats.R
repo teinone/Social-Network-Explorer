a68dccf30c417333ebcd20a339be44fb00d77e7f
teinone <pauli.teinonen@gmail.com>
2019-03-12 18:31:35 +0100

    Fixed naming, formatting.
    Renamed to f.stats

---
+++ folder/dev/f.stats.R
@@ -0,0 +1,25 @@
+#==============================================================================
+#FUNCTION: Network stats
+#==============================================================================
+
+f.stats <- function(g){
+  g <- c(
+    number.of.degree <-  as.numeric(length(E(g))),
+    number.of.nodes <- as.numeric(length(V(g))),
+    average.degree.number <- mean(degree(g)),
+    average.path.length <- average.path.length(g),
+    median.degree <- median(degree(g)),
+    sd.degree <- sd(degree(g)))
+  
+  g <- as.data.table(g)
+  g$rownames <-rownames(g) <- c("number of degree",
+                                "number of nodes",
+                                "average degree number",
+                                "avarage path lenghth",
+                                "median degee",
+                                "standard degree")
+  setnames(g, c("g", "rownames"), c("values","measures"))
+  
+  g <- g[,c("measures", "values")]
+  return(g)
+}
