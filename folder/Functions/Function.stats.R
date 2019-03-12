e790f1fe82628a6cd111804f660473994b60cf52
CuriousGeorge7265 <47153667+CuriousGeorge7265@users.noreply.github.com>
2019-03-12 12:54:59 +0100

    Update Function.stats.R

--- folder/Functions/Function.stats.R
+++ folder/Functions/Function.stats.R
@@ -1,21 +1,16 @@
-load("/Users/Desktop/NDATeam12/reddit.masters.RData")
-library(igraph)
-library(data.table)
 stats <- function(g){
   g <- c(
-    number.of.degree <-  as.numeric(length(degree(g))),
+    number.of.degree <-  as.numeric(length(E(g))),
     number.of.nodes <- as.numeric(length(V(g))),
     average.degree.number <- mean(degree(g)),
     average.path.length <- average.path.length(g),
     median.degree <- median(degree(g)),
-    diameter <- diameter(g),
     sd.degree <- sd(degree(g))
   )
-  g <- data.frame(g, row.names=c("number of degree", "number of nodes", "average degree number", "avarage path lenghth", "median degee", "diameter", "standard degree"))
+  g <- as.data.table(g)
+  g$rownames <-rownames(g) <- c("number of degree", "number of nodes", "average degree number", "avarage path lenghth", "median degee", "standard degree")
+  setnames(g, c("g", "rownames"), c("values","measures"))
+  
+  g <- g[,c("measures", "values")]
   return(g)
 }
-
-
-
-
-         
